b644ec2303856c5de3970947713cd9e7044b4cd3
A2XI <aluiion.iulian@gmail.com>
2022-04-03 18:04:33 +0300

    Changing the database

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "password";
-$database = "first_db";
+$servername = "eu-cdbr-west-02.cleardb.net";
+$username = "beb94576cfbf20";
+$password = "2cca6af1";
+$database = "heroku_9c278d4e8255f6b";
 
 $conn = new mysqli($servername, $username, $password, $database) or die($conn->error);
 ?>
